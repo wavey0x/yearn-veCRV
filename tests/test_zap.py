29b297e350a40d2586e543ef15e136e3f7be1b28
wavey0x <retrowaveyy@gmail.com>
2022-06-18 19:38:00 -0400

    feat: test zap

--- tests/test_zap.py
+++ tests/test_zap.py
@@ -7,9 +7,9 @@ def test_zap(zap, pool, Strategy, accounts, voter, whale_crv, token, yveCrv, yvb
     yvboost.approve(zap, 2**256-1, {"from": user})
     yveCrv.approve(zap, 2**256-1, {"from": user})
     
-    # assert False
-    crv.transfer(zap,2e18,{'from':user})
-    # zap.zapCRVtoYvBOOST(2e18, 0, user, {"from":user})
     zap.zapCRVtoLPVault(2e18, 0, user, {"from":user})
+    zap.zapCRVtoYvBOOST(2e18, 0, user, {"from":user})
+    zap.zapYvBOOSTtoLPVault(2e18, 0, user, {"from":user})
+    
     # pool.exchange(0,1,2e18,0,{'from':user})
     
