1de975e4a0b0c416d671d7f3340d06ee110862d4
wavey0x <retrowaveyy@gmail.com>
2022-06-19 13:06:17 -0400

    feat: audit start

--- tests/test_zap.py
+++ tests/test_zap.py
@@ -10,7 +10,10 @@ def test_zap(zap, pool, Strategy, accounts, voter, yvLP, whale_crv, token, yveCr
     
     crv_before = crv.balanceOf(user)
     yv_before = yvLP.balanceOf(user)
+    print("ZAP CRV --> LP VAULT\n")
+    print_balances(user, yvLP, crv, yvboost, yveCrv, pool)
     zap.zapCRVtoLPVault(2e18, 0, user, {"from":user})
+    print_balances(user, yvLP, crv, yvboost, yveCrv, pool)
     crv_after = crv.balanceOf(user)
     yv_after = yvLP.balanceOf(user)
     assert yv_after > yv_before
@@ -18,7 +21,10 @@ def test_zap(zap, pool, Strategy, accounts, voter, yvLP, whale_crv, token, yveCr
 
     crv_before = crv.balanceOf(user)
     yvboost_before = yvboost.balanceOf(user)
+    print("ZAP CRV --> YVBOOST\n")
+    print_balances(user, yvLP, crv, yvboost, yveCrv, pool)
     zap.zapCRVtoYvBOOST(2e18, 0, user, {"from":user})
+    print_balances(user, yvLP, crv, yvboost, yveCrv, pool)
     crv_after = crv.balanceOf(user)
     yvboost_after = yvboost.balanceOf(user)
     assert yvboost_after > yvboost_before
@@ -26,7 +32,10 @@ def test_zap(zap, pool, Strategy, accounts, voter, yvLP, whale_crv, token, yveCr
 
     yvLP_before = yvLP.balanceOf(user)
     yvboost_before = yvboost.balanceOf(user)
+    print("ZAP YVBOOST --> LP VAULT\n")
+    print_balances(user, yvLP, crv, yvboost, yveCrv, pool)
     zap.zapYvBOOSTtoLPVault(2e18, 0, user, {"from":user})
+    print_balances(user, yvLP, crv, yvboost, yveCrv, pool)
     yvLP_after = yvLP.balanceOf(user)
     yvboost_after = yvboost.balanceOf(user)
     assert yvboost_before > yvboost_after
@@ -34,7 +43,10 @@ def test_zap(zap, pool, Strategy, accounts, voter, yvLP, whale_crv, token, yveCr
 
     yvLP_before = yvLP.balanceOf(user)
     yvboost_before = yvboost.balanceOf(user)
+    print("ZAP LP VAULT --> YVBOOST\n")
+    print_balances(user, yvLP, crv, yvboost, yveCrv, pool)
     zap.zapLPVaultToYvBOOST(1e18, 0, user, {"from":user})
+    print_balances(user, yvLP, crv, yvboost, yveCrv, pool)
     yvLP_after = yvLP.balanceOf(user)
     yvboost_after = yvboost.balanceOf(user)
     assert yvboost_before < yvboost_after
@@ -42,3 +54,9 @@ def test_zap(zap, pool, Strategy, accounts, voter, yvLP, whale_crv, token, yveCr
 
     # pool.exchange(0,1,2e18,0,{'from':user})
     
+def print_balances(user, yvLP, crv, yvboost, yveCrv, pool):
+    print("CRV:", crv.balanceOf(user)/1e18)
+    print("yveCRV:", yveCrv.balanceOf(user)/1e18)
+    print("yvBOOST:", yvboost.balanceOf(user)/1e18)
+    print("yvLP:", yvLP.balanceOf(user)/1e18)
+    print("----------\n")
