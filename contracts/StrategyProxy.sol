d52bd7d69eea20c4ef7b63b16afc5465b6f4202e
dudesahn <23222916+dudesahn@users.noreply.github.com>
2022-11-11 19:30:46 -0500

    feat: proxy updates for factory

--- contracts/StrategyProxy.sol
+++ contracts/StrategyProxy.sol
@@ -1,16 +1,16 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL-3.0
 
 pragma solidity ^0.6.12;
 
-import "@openzeppelinV3/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelinV3/contracts/math/SafeMath.sol";
-import "@openzeppelinV3/contracts/utils/Address.sol";
-import "@openzeppelinV3/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelinLegacy/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelinLegacy/contracts/utils/Address.sol";
+import "@openzeppelinLegacy/contracts/math/Math.sol";
+import "@openzeppelinLegacy/contracts/token/ERC20/SafeERC20.sol";
 
-import "../interfaces/yearn/IProxy.sol";
-import "../interfaces/curve/Mintr.sol";
-import "../interfaces/curve/FeeDistribution.sol";
-import "../interfaces/curve/Gauge.sol";
+import "./interfaces/IProxy.sol";
+import "./interfaces/Mintr.sol";
+import "./interfaces/FeeDistribution.sol";
+import "./interfaces/Gauge.sol";
 
 library SafeProxy {
     function safeExecute(
@@ -35,15 +35,25 @@ contract StrategyProxy {
     using SafeProxy for IProxy;
 
     uint256 private constant week = 604800; // Number of seconds in a week
-    IProxy public constant proxy = IProxy(0xF147b8125d2ef93FB6965Db97D6746952a133934); // Refer to this as "voter" contract
-    address public constant mintr = address(0xd061D61a4d941c39E5453435B6345Dc261C2fcE0);
-    address public constant crv = address(0xD533a949740bb3306d119CC777fa900bA034cd52);
-    address public constant gauge = address(0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB);
-    address public constant yveCRV = address(0xc5bDdf9843308380375a611c18B50Fb9341f502A);
-    address public constant CRV3 = address(0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490);
-    address public feeRecipient = address(0xc5bDdf9843308380375a611c18B50Fb9341f502A); // Default value is yveCRV
-    FeeDistribution public constant feeDistribution = FeeDistribution(0xA464e6DCda8AC41e03616F95f4BC98a13b8922Dc);
-    VeCRV public constant veCRV  = VeCRV(0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2);
+    IProxy public constant proxy =
+        IProxy(0xF147b8125d2ef93FB6965Db97D6746952a133934); // Refer to this as "voter" contract
+    address public constant mintr =
+        address(0xd061D61a4d941c39E5453435B6345Dc261C2fcE0);
+    address public constant crv =
+        address(0xD533a949740bb3306d119CC777fa900bA034cd52);
+    address public constant gauge =
+        address(0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB);
+    address public constant yveCRV =
+        address(0xc5bDdf9843308380375a611c18B50Fb9341f502A);
+    address public constant CRV3 =
+        address(0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490);
+    address public feeRecipient =
+        address(0xc5bDdf9843308380375a611c18B50Fb9341f502A); // Default value is yveCRV
+    address public factory; // our permissionless curve vault factory
+    FeeDistribution public constant feeDistribution =
+        FeeDistribution(0xA464e6DCda8AC41e03616F95f4BC98a13b8922Dc);
+    VeCRV public constant veCRV =
+        VeCRV(0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2);
     // gauge => strategies
     mapping(address => address) public strategies;
     mapping(address => bool) public voters;
@@ -54,6 +64,7 @@ contract StrategyProxy {
 
     // Events so that indexers can keep track of key actions
     event GovernanceSet(address governance);
+    event FactorySet(address factory);
     event FeeRecipientSet(address feeRecipient);
     event StrategyApproved(address gauge, address strategy);
     event StrategyRevoked(address gauge);
@@ -76,6 +87,15 @@ contract StrategyProxy {
         emit GovernanceSet(_governance);
     }
 
+    /// @notice Set curve vault factory address
+    /// @param _factory Address to set as curve vault factory
+    function setFactory(address _factory) external {
+        require(msg.sender == governance, "!governance");
+        require(_factory != factory, "already set");
+        factory = _factory;
+        emit FactorySet(_factory);
+    }
+
     /// @notice Set recipient of weekly 3CRV admin fees
     /// @dev Only a single address can be approved at any time
     /// @param _feeRecipient Address to approve for fees
@@ -91,7 +111,10 @@ contract StrategyProxy {
     /// @param _gauge Gauge to permit strategy on
     /// @param _strategy Strategy to approve on gauge
     function approveStrategy(address _gauge, address _strategy) external {
-        require(msg.sender == governance, "!governance");
+        require(
+            msg.sender == governance || msg.sender == factory,
+            "!governance"
+        );
         require(_strategy != address(0), "disallow zero");
         require(strategies[_gauge] != _strategy, "already approved");
         strategies[_gauge] = _strategy;
@@ -154,9 +177,12 @@ contract StrategyProxy {
     function maxLock() external {
         require(msg.sender == governance || lockers[msg.sender], "!locker");
         proxy.safeExecute(
-            address(veCRV), 
-            0, 
-            abi.encodeWithSignature("increase_unlock_time(uint256)", now + (365 days * 4))
+            address(veCRV),
+            0,
+            abi.encodeWithSignature(
+                "increase_unlock_time(uint256)",
+                now + (365 days * 4)
+            )
         );
     }
 
@@ -171,16 +197,27 @@ contract StrategyProxy {
     /// @notice Vote on a multiple gauges
     /// @param _gauges List of gauges to vote on
     /// @param _weights List of weight to vote with
-    function vote_many(address[] calldata _gauges, uint256[] calldata _weights) external {
+    function vote_many(
+        address[] calldata _gauges,
+        uint256[] calldata _weights
+    ) external {
         require(msg.sender == governance || voters[msg.sender], "!voter");
         require(_gauges.length == _weights.length, "!mismatch");
-        for(uint256 i = 0; i < _gauges.length; i++) {
+        for (uint256 i = 0; i < _gauges.length; i++) {
             _vote(_gauges[i], _weights[i]);
         }
     }
 
     function _vote(address _gauge, uint256 _weight) internal {
-        proxy.safeExecute(gauge, 0, abi.encodeWithSignature("vote_for_gauge_weights(address,uint256)", _gauge, _weight));
+        proxy.safeExecute(
+            gauge,
+            0,
+            abi.encodeWithSignature(
+                "vote_for_gauge_weights(address,uint256)",
+                _gauge,
+                _weight
+            )
+        );
     }
 
     /// @notice Withdraw exact amount of LPs from gauge
@@ -194,9 +231,21 @@ contract StrategyProxy {
     ) public returns (uint256) {
         require(strategies[_gauge] == msg.sender, "!strategy");
         uint256 _balance = IERC20(_token).balanceOf(address(proxy));
-        proxy.safeExecute(_gauge, 0, abi.encodeWithSignature("withdraw(uint256)", _amount));
+        proxy.safeExecute(
+            _gauge,
+            0,
+            abi.encodeWithSignature("withdraw(uint256)", _amount)
+        );
         _balance = IERC20(_token).balanceOf(address(proxy)).sub(_balance);
-        proxy.safeExecute(_token, 0, abi.encodeWithSignature("transfer(address,uint256)", msg.sender, _balance));
+        proxy.safeExecute(
+            _token,
+            0,
+            abi.encodeWithSignature(
+                "transfer(address,uint256)",
+                msg.sender,
+                _balance
+            )
+        );
         return _balance;
     }
 
@@ -209,7 +258,10 @@ contract StrategyProxy {
     /// @notice Withdraw full balance of voter's LPs from gauge
     /// @param _gauge The gauge from which to withdraw
     /// @param _token The LP token to withdraw from gauge
-    function withdrawAll(address _gauge, address _token) external returns (uint256) {
+    function withdrawAll(
+        address _gauge,
+        address _token
+    ) external returns (uint256) {
         return withdraw(_gauge, _token, balanceOf(_gauge));
     }
 
@@ -223,9 +275,25 @@ contract StrategyProxy {
         IERC20(_token).safeTransfer(address(proxy), _balance);
         _balance = IERC20(_token).balanceOf(address(proxy));
 
-        proxy.safeExecute(_token, 0, abi.encodeWithSignature("approve(address,uint256)", _gauge, 0));
-        proxy.safeExecute(_token, 0, abi.encodeWithSignature("approve(address,uint256)", _gauge, _balance));
-        proxy.safeExecute(_gauge, 0, abi.encodeWithSignature("deposit(uint256)", _balance));
+        proxy.safeExecute(
+            _token,
+            0,
+            abi.encodeWithSignature("approve(address,uint256)", _gauge, 0)
+        );
+        proxy.safeExecute(
+            _token,
+            0,
+            abi.encodeWithSignature(
+                "approve(address,uint256)",
+                _gauge,
+                _balance
+            )
+        );
+        proxy.safeExecute(
+            _gauge,
+            0,
+            abi.encodeWithSignature("deposit(uint256)", _balance)
+        );
     }
 
     /// @notice Abstracts the CRV minting and transfers to an approved strategy with CRV earnings
@@ -234,9 +302,21 @@ contract StrategyProxy {
     function harvest(address _gauge) external {
         require(strategies[_gauge] == msg.sender, "!strategy");
         uint256 _balance = IERC20(crv).balanceOf(address(proxy));
-        proxy.safeExecute(mintr, 0, abi.encodeWithSignature("mint(address)", _gauge));
+        proxy.safeExecute(
+            mintr,
+            0,
+            abi.encodeWithSignature("mint(address)", _gauge)
+        );
         _balance = (IERC20(crv).balanceOf(address(proxy))).sub(_balance);
-        proxy.safeExecute(crv, 0, abi.encodeWithSignature("transfer(address,uint256)", msg.sender, _balance));
+        proxy.safeExecute(
+            crv,
+            0,
+            abi.encodeWithSignature(
+                "transfer(address,uint256)",
+                msg.sender,
+                _balance
+            )
+        );
     }
 
     /// @notice Claim share of weekly admin fees from Curve fee distributor
@@ -247,12 +327,22 @@ contract StrategyProxy {
         if (!claimable()) return;
 
         address p = address(proxy);
-        feeDistribution.claim_many([p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p]);
+        feeDistribution.claim_many(
+            [p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p]
+        );
         lastTimeCursor = feeDistribution.time_cursor_of(address(proxy));
 
         uint256 amount = IERC20(CRV3).balanceOf(address(proxy));
         if (amount > 0) {
-            proxy.safeExecute(CRV3, 0, abi.encodeWithSignature("transfer(address,uint256)", _recipient, amount));
+            proxy.safeExecute(
+                CRV3,
+                0,
+                abi.encodeWithSignature(
+                    "transfer(address,uint256)",
+                    _recipient,
+                    amount
+                )
+            );
             emit AdminFeesClaimed(_recipient, amount);
         }
     }
@@ -265,10 +355,21 @@ contract StrategyProxy {
 
     /// @notice Claim non-CRV token incentives from the gauge and transfer to strategy
     /// @param _gauge The gauge which this strategy is claiming rewards
-    /// @param _token The token to be claimed to the approved strategy
-    function claimRewards(address _gauge, address _token) external {
+    /// @param _tokens The token(s) to be claimed to the approved strategy
+    function claimRewards(address _gauge, address[] memory _tokens) external {
         require(strategies[_gauge] == msg.sender, "!strategy");
         Gauge(_gauge).claim_rewards(address(proxy));
-        proxy.safeExecute(_token, 0, abi.encodeWithSignature("transfer(address,uint256)", msg.sender, IERC20(_token).balanceOf(address(proxy))));
+        for (uint256 i; i < _tokens.length; ++i) {
+            address _token = _tokens[i];
+            proxy.safeExecute(
+                _token,
+                0,
+                abi.encodeWithSignature(
+                    "transfer(address,uint256)",
+                    msg.sender,
+                    IERC20(_token).balanceOf(address(proxy))
+                )
+            );
+        }
     }
 }
